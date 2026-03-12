e1a523b2da9ed9203f864e8de325145748a83752
Dobrosvet <dimaprokofev@yandex.ru>
2026-03-12 08:54:28 +0300

    Исправление деплоя.

--- client/docker-entrypoint-custom.sh
+++ client/docker-entrypoint-custom.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+set -e
+
 # Получаем DNS resolver из /etc/resolv.conf
 RESOLVER=$(grep -m1 "^nameserver" /etc/resolv.conf | awk '{print $2}')
 if [ -z "$RESOLVER" ]; then
@@ -6,7 +8,18 @@ if [ -z "$RESOLVER" ]; then
 fi
 echo "Using DNS resolver: $RESOLVER"
 
-# Подставляем resolver в nginx.conf
+# Запускаем стандартные nginx entrypoint скрипты (envsubst и т.д.)
+# кроме финального запуска nginx
+/docker-entrypoint.d/10-listen-on-ipv6-by-default.sh
+. /docker-entrypoint.d/15-local-resolvers.envsh
+/docker-entrypoint.d/20-envsubst-on-templates.sh
+/docker-entrypoint.d/30-tune-worker-processes.sh
+
+# Теперь подставляем resolver в уже сгенерированный конфиг
 sed -i "s/RESOLVER_PLACEHOLDER/$RESOLVER/g" /etc/nginx/conf.d/default.conf
 
-exec "$@"
+echo "Final nginx config resolver line:"
+grep "resolver" /etc/nginx/conf.d/default.conf
+
+# Запускаем nginx
+exec nginx -g "daemon off;"
